589dd406c52fd16a859095ff89e2344a70b0cd69
Weon Seok Choi <wc25@gold34.cs.calvin.edu>
2018-09-21 14:40:55 -0400

    Hello

---
+++ Lab02/app/src/main/java/wc25/cs262/calvin/edu/lab2re/MainActivity.java
@@ -0,0 +1,23 @@
+package wc25.cs262.calvin.edu.lab2re;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void countUp(View view) {
+        // What happens when user clicks on the button_count Button goes here.
+    }
+
+
+    public void showToast(View view) {
+        // What happens when user clicks on the button_toast Button goes here.
+
+    }
+}
